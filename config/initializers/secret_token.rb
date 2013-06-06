63a155f5ab2fbeab2da2c97963d1e0ecf7d4d46b
Colin Shea <colin@evaryont.me>
2013-06-05 21:55:39 -0700

    Brand new Rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fisheye::Application.config.secret_token = 'a8fc3703b9be99ac1c7d4c60631a7c3513ca09d0eaf2d1411213665be4bb4d08e03d1bbb98daaf1ec83da5f71a9d81006cbdd2a379d6819ff179eb05e1992a3a'
