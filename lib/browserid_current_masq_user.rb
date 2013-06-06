2d6b818c3e53b8c450ef5e240529e36e0d7d13ef
Colin Shea <colin@evaryont.me>
2013-06-06 03:28:44 -0700

    Monkeypatch BrowserID::Provider
    One is to integrate BrowserID::Provider with Masq's user session data. The other
    is to fix a bug until >0.5.6 comes out. (The configuration parameters are reset
    without the hotfix to initialize.)

--- lib/browserid_current_masq_user.rb
+++ lib/browserid_current_masq_user.rb
@@ -1,10 +1,15 @@
 # Monkey-patch BrowserID::Provider to use Masq's authentication instead of
 # warden.
-require 'pp'
+require 'browserid-provider/provider'
 module BrowserID
   class Provider
+    def initialize(app = nil, options = {})
+      @app, @config = app, BrowserID::Config.new(options)
+      @identity = BrowserID::Identity.new(options)
+    end
+
+    private
     def current_user_email
-      pp @env
       'bob@example.com'
     end
   end
