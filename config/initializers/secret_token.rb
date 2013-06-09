f13a68fe23208ef88afcce607904738b4bd85281
Colin Shea <colin@evaryont.me>
2013-06-08 23:20:15 -0700

    A clever way to auto-configure the secret token
    May not work so well across multiple copies of this application (horizontal
    scaling) so...uh... welp. Anyways, bleh!

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,18 @@
-# Be sure to restart your server when you modify this file.
-
 # Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Fisheye::Application.config.secret_token = 'a8fc3703b9be99ac1c7d4c60631a7c3513ca09d0eaf2d1411213665be4bb4d08e03d1bbb98daaf1ec83da5f71a9d81006cbdd2a379d6819ff179eb05e1992a3a'
+
+if File.exists? "#{Fisheye::Application.config.root}/tmp/cookie_token.rb"
+  # Load the tokens stored in the temporary file
+  require "#{Fisheye::Application.config.root}/tmp/cookie_token.rb"
+else
+  # If there isn't a secret token yet, create one automatically.
+  if Fisheye::Application.config.secret_token.blank?
+    require 'securerandom'
+    Fisheye::Application.config.secret_token = SecureRandom.hex(64)
+  end
+
+  # A cache file ensures the tokens will stay static across restarts.
+  File.open("#{Fisheye::Application.config.root}/tmp/cookie_token.rb", 'w') do |token_cache|
+    token_cache.puts "Fisheye::Application.config.secret_token = '#{Fisheye::Application.config.secret_token}'"
+  end
+end
+
